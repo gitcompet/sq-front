96342b3fb1afbd6506781b31929021f6449dffad
adliad <s.adli@aubay.lu>
2023-03-03 15:22:35 +0100

    Fix property error

--- src/app/features/user-profile/components/signup/sign-up.component.ts
+++ src/app/features/user-profile/components/signup/sign-up.component.ts
@@ -9,32 +9,9 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 export class SignUpComponent {
   constructor(public authService: AuthService) {}
   ngOnInit() {
-    this.authService.formModel.reset();
-  }
-  onsubmit1() {
-    this.authService.register().subscribe(
-      (res: any) => {
-        if (res.succeded) {
-          this.authService.formModel.reset();
-          console.log('res:' + res);
-        }
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
-  onsubmit() {
-    this.authService.register().subscribe({
-      next: (res) => {
-        console.log('res :', res);
-        if (res) {
-          this.authService.formModel.reset();
-        }
-      },
 
-      error: (err) => console.log(err),
-      complete: () => console.log('completed'),
-    });
+  }
+   onsubmit() {
+
   }
 }
