b291283de036c9d319a93543fee45053a4df075e
adliad <s.adli@aubay.lu>
2023-03-15 11:11:34 +0100

    Add env files

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,8 +1,7 @@
 (function (window) {
     window['env'] = window['env'] || {};
-  
+
     // Environment variables
     window['env']['URL_API'] = 'http://145.239.0.38';
-    window['env']['PORT_API'] = '5212';
+    window['env']['PORT_API'] = '9999';
   })(this);
-  
