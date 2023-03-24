a444fdc8fbcf23da195e801beac9a5a0fe7a9a03
adliad <s.adli@aubay.lu>
2023-03-24 11:37:03 +0100

    fix merges

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,10 +2,6 @@
     window['env'] = window['env'] || {};
 
     // Environment variables
-<<<<<<< HEAD
-    window['env']['URL_API'] = 'http://145.239.0.38';
-=======
     window['env']['URL_API'] = 'http://localhost';
->>>>>>> developement
     window['env']['PORT_API'] = '9999';
   })(this);
